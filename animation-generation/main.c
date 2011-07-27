83b3ba621edf292f4d252739a0ef0a53402e2bf9
Alexander Morozov <alexander@MacBook-Alexander-Morozov.local>
2011-07-27 12:28:21 +0700

    node

--- animation-generation/main.c
+++ animation-generation/main.c
@@ -10,7 +10,14 @@
 
 int main (int argc, const char * argv[])
 {
-
+    FILE* f;
+    int i ;
+    f=fopen("/animation-map.txt", "w");
+    for(i=1;i<=10;i++)
+    {
+        fprintf(f, "<dict>\n<key>count</key>\n<integer>1</integer>\n<key>name</key>\n<string>play-menu-buttons-sound-select%.4d.png</string>\n</dict>\n",i);
+    }
+    fclose(f);
     // insert code here...
     printf("Hello, World!\n");
     return 0;
